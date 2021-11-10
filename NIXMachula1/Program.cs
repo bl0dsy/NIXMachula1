a5aae3964d2a95abbefb5d540ab75eacec929ca6
juliatkachenko99 <juliatkachenko99@gmail.com>
2021-11-10 02:48:11 +0200

    Add project files.

---
+++ NIXMachula1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NIXMachula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Задание 1. Найдите площадь и периметр прямоугольника?");
+            double sideA, sideB, area, perimeter;
+            Console.WriteLine("Введите первое значение");
+            sideA = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе значение");
+            sideB = double.Parse(Console.ReadLine());
+            area = sideA * sideB;
+            perimeter = (sideA + sideB) * 2;
+            Console.WriteLine($"Площадь = { area }");
+            Console.WriteLine($"Периметр = {perimeter}");
+
+        }
+    }
+}
